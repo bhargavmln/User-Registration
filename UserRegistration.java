18653fa2fe0627ffabe33b7a340f64929b8b06d9
bhargavmln <bhargavmln@gmail.com>
2020-10-04 00:21:34 +0530

    [bhargavmln] update: updated the email validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -23,7 +23,7 @@ public class UserRegistration{
 		
 		System.out.println("Enter the Email: ");
 		String email = sc.next();
-		while(!Pattern.matches("^[A-Za-z0-9]+(\\.[A-Za-z0-9-]+)?@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)?(\\.[A-Za-z]{2,})$",email)){
+		while(!Pattern.matches("^[A-Za-z0-9]+([\\+\\.-][A-Za-z0-9-]+)?@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)?(\\.[A-Za-z]{2,})$",email)){
 			System.out.println("Ivalid input. Enter the Email: ");
 			email = sc.next();
 		}
