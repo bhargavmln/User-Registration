22afa0cdabf70e3395f43171ddb26b22f352af07
bhargavmln <bhargavmln@gmail.com>
2020-10-03 23:11:02 +0530

    [bhargavmln]refactor: validation for minimum 1 uppercase added

--- UserRegistration.java
+++ UserRegistration.java
@@ -39,7 +39,7 @@ public class UserRegistration{
 		
 		System.out.println("Enter the Password: ");
 		String password = sc.next();
-		while(!Pattern.matches(".{8,}",password)){
+		while(!Pattern.matches("^(?=.*[A-Z]).{8,}$",password)){
 			System.out.println("Ivalid input. Enter the Password: ");
 			password = sc.nextLine();
 		}	
